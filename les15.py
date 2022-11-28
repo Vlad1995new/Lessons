2748ec891958e46d00ed7f1be1e5a7e86d83ca9f
Vlad1995new <Vlad.Lapeonok@gmail.com>
2022-11-28 18:01:03 +0300

    create file les 15

--- les15.py
+++ les15.py
@@ -1,6 +1,8 @@
 # В одномерном массиве целых чисел определить два наименьших элемента.
 # Они могут быть как равны между собой (оба являться минимальными), так и различаться
-list1 = list(input())
+import random
+list1 = list(random.randint(1, 100) for i in range(10))
+print(list1)
 list1.sort()
 print(list1)
 list1.pop(0)
